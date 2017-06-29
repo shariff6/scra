7caef18c5cdcbd1fd6d4c1264cfa98b0a9cfa3c1
shariff6 <shariff.mohammed.a@gmail.com>
2017-06-29 16:32:27 +0300

    added test to check if values are added

--- spec/srabble_score_spec.rb
+++ spec/srabble_score_spec.rb
@@ -5,4 +5,7 @@ describe("String#scrabble_score") do
   it("gets the value of the letters")do
       expect("a".scrabble_score()).to(eq(1))
     end
+    it ("gets the value of each letter and adds them together") do
+      expect("abc".scrabble_score()).to(eq(7))
+    end
   end
